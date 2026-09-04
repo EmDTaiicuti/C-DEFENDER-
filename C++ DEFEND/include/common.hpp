e0887f7b2d49f431c4233f2c4834b3962165fdf3
zhaahue08 <huegia08@gmail.com>
2026-09-04 22:39:02 +0700

    Update common.hpp

--- C++ DEFEND/include/common.hpp
+++ C++ DEFEND/include/common.hpp
@@ -1,1 +1,21 @@
+#pragma once
+#include <raylib.h>
 
+const int TILE_SIZE = 40;
+const int GRID_WIDTH = 20;   
+const int GRID_HEIGHT = 15;  
+
+enum TileType {
+    EMPTY,       
+    PATH,        
+    OBSTACLE,    
+    SPAWN,       
+    BASE         
+};
+
+struct Point {
+    int x, y;
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+};
